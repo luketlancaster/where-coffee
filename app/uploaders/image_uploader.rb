446253d916fbb34089276d0b6d325f56ae4bafd0
Luke Lancaster <luketlancaster@gmail.com>
2015-06-25 13:49:20 -0500

    fix uploader to make files smaller

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,7 +25,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  #process :resize_to_limit => [380, 168]
+  process :resize_to_limit => [400, 482]
   #
   # def scale(width, height)
   #   # do something
